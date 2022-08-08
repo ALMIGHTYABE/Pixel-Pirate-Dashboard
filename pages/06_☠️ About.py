8d0b6348ac465980e0875b3dd7ec5c9e4ce1dd65
ALMIGHTY ABE <5318008bb@gmail.com>
2022-08-08 15:47:53 +0530

    Add About Page

---
+++ pages/06_☠️ About.py
@@ -0,0 +1,42 @@
+# Importing Libraries
+import streamlit as st
+
+# App
+st.set_page_config(
+    page_title="About",
+    page_icon="icons/pp.png",
+    layout="wide",
+)
+
+# Title
+st.title("☠️ About")
+
+# Content
+st.write("""## What is Pixel Pirates?
+Pixel Pirates (PPs) are the brainchild of the legendary Hoeem. Together with the expertise of Hedgey Finance, Hoeem brought to life a wildly popular project with a large supporting community. 🤺
+
+Simply put it is a generative art collection of pirates all of which live on the blockchain all with 1 cLQDR inside; as cLQDR auto-compounds and tracks the price of LQDR these NFTs have a base floor attached to how we value them. The NFT can be burnt through Hedgey.finance to accrue to the underlying token it is backed by. 💰
+
+Along with that, there are also various giveaways and competitions that are running where your Pixel Pirate traits are needed to compete in! 😛
+
+The Pirate Community are always looking for new crew mates on board, they usually hang out in both the LiquidDriver Discord “NFT-Lounge” and in the Hedgey Finance discord! Come say hello if you have any questions to ask them! 😉 
+
+## Why this Tracker?
+This was created by me to help Pixel Pirate holders view their collection, check out the various attributes they hold, follow sales on Secondary, and more.
+
+## Social Links
+LiquidDriver Discord: https://discord.gg/liquiddriver
+
+Hedgey Finance Discord: https://discord.gg/hedgey
+
+Twitter community:
+https://twitter.com/i/communities/1529078922847068160
+
+Secondary market:
+https://paintswap.finance/marketplace/collections/0x2aa5d15eb36e5960d056e8fea6e7bb3e2a06a351
+
+Hoeem's Twitter: https://twitter.com/crypthoem/
+
+My Twitter: https://twitter.com/KingAbraham69
+""")
+
